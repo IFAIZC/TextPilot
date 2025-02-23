b5dcbaf948416b752f1c090afbcf53d3c7dc01d6
Irfan Faiz Choo <165414147+IFAIZC@users.noreply.github.com>
2025-02-23 18:30:24 +0800

    implementing new tab for custom input

--- script.js
+++ script.js
@@ -22,3 +22,45 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+// adding custom user input
+
+// function submitInput() {
+
+//   let title = document.getElementById("title").value;
+//   let input = document.getElementById("input").value;
+
+//   if (title === "" || input === "") {
+//     alert("Please fill in both fields!");
+//     return;
+//   }
+
+//   // Create a new <p> element
+//   let p = document.createElement("p");
+//   p.innerHTML = `<strong>${title}</strong> ${input} `;
+
+//   // Pushing new inputs to the arrays (customInput)
+//   // let newEntry = { title : title, content: input };
+//   // customInput.push(newEntry);
+
+//   // Create a "Copy" button
+//   let copyButton = document.createElement("button");
+//   copyButton.textContent = "Copy";
+//   copyButton.addEventListener("click", function () {
+//       navigator.clipboard.writeText(`${input}`);
+//       alert("Copied to clipboard!");
+//   });
+
+//   // Append button to <p>
+//   p.appendChild(copyButton);
+
+//   // Append <p> to the output div
+//   document.getElementById("output").appendChild(p);
+  
+//   // Clear input fields after submission
+//   document.getElementById("title").value = "";
+//   document.getElementById("input").value = "";
+// }
+
+// // making the addButton runs the function submitInput()
+// document.getElementById("addButton").addEventListener("click", submitInput);
